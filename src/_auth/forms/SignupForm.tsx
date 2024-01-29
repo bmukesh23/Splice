3d3e8eae717aa55c987cd50752460f5439c4f3cc
Mukesh <billamukesh1234@gmail.com>
2024-01-29 22:31:26 +0530

    style: signup form

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,8 +1,9 @@
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Link } from "react-router-dom";
+
 import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
-
 import {
   Form, FormControl, FormField, FormItem, FormMessage,
 } from "@/components/ui/form"
@@ -10,6 +11,7 @@ import { Input } from "@/components/ui/input"
 import { SignupValidation } from "@/lib/validation";
 
 const SignupForm = () => {
+  const isLoading = false;
 
   const form = useForm<z.infer<typeof SignupValidation>>({
     resolver: zodResolver(SignupValidation),
@@ -21,15 +23,17 @@ const SignupForm = () => {
     },
   })
 
-  function onSubmit(values: z.infer<typeof SignupValidation>) {
-    console.log(values)
+  const onSubmit = async (values: z.infer<typeof SignupValidation>) => {
+    // const newUser = await createUserAccount(values);
+    console.log(values);
+    
   }
 
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <h1 className="h1-bold md:h2-bold">splice</h1>
-        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
+        <h1 className="h1-bold">splice</h1>
+        <h2 className="h3-bold md:h3-bold pt-4 sm:pt-4">Create a new account</h2>
         <p className="text-light-3 small-medium md:base-regular mt-2">Sign up to see photos and videos from your friends</p>
         <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
           <FormField
@@ -80,7 +84,18 @@ const SignupForm = () => {
               </FormItem>
             )}
           />
-          <Button type="submit" className="shad-button_primary">Sign up</Button>
+          <Button type="submit" className="shad-button_primary">
+            {isLoading ? (
+              <div className="flex-center gap-2">
+                {/* <Loader/> */}
+                Loading...
+              </div>
+            ) : "Sign up"}
+          </Button>
+          <p className="text-small-regular text-light-2 text-center mt-2">
+            Have an account?
+            <Link to='/sign-in' className="text-primary-500 text-small-semibold ml-1">Log in</Link>
+          </p>
         </form>
       </div>
     </Form>
