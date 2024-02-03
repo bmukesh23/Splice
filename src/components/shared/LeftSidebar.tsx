b7eb99de5113687de21255ca999c57a57ec9d051
Mukesh <billamukesh1234@gmail.com>
2024-02-03 23:31:19 +0530

    refactor: sidebars

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,8 +1,10 @@
 import { useSignOutAccount } from "@/lib/react-query/queriesAndMutation"
-import { Link, useNavigate } from "react-router-dom"
+import { Link, NavLink, useNavigate } from "react-router-dom"
 import { Button } from "../ui/button";
 import { useEffect } from "react";
 import { useUserContext } from "@/context/AuthContext";
+import { sidebarLinks } from "@/constants";
+import { INavLink } from "@/types";
 
 const LeftSidebar = () => {
   const { mutate: signOut, isSuccess } = useSignOutAccount();
@@ -33,7 +35,23 @@ const LeftSidebar = () => {
         </Link>
 
         <ul className="flex flex-col gap-6">
-          
+          {sidebarLinks.map((link: INavLink) => {
+            return (
+              <li key={link.label} className="leftsidebar-link">
+                <NavLink
+                  to={link.route}
+                  className="flex gap-4 items-center p-4"
+                >
+                  {/* <img
+                    src={link.imgURL}
+                    alt={link.label}
+                    className="group-hover:invert-white"
+                  /> */}
+                  {link.label}
+                </NavLink>
+              </li>
+            )
+          })}
         </ul>
       </div>
     </nav>
