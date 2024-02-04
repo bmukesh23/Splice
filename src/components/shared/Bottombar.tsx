379b8255f653fb7e2e0f9764f504ee79c9a2bf23
Mukesh <billamukesh1234@gmail.com>
2024-02-04 18:37:40 +0530

    imported bottom-bar

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,6 +1,31 @@
+import { bottombarLinks } from "@/constants";
+import { Link, useLocation } from "react-router-dom";
+
+
 const Bottombar = () => {
+  const { pathname } = useLocation();
   return (
-    <div>Bottombar</div>
+    <section className="z-50 flex-between w-full sticky bottom-0 rounded-t-[20px] bg-dark-2 px-5 py-4 md:hidden">
+      {bottombarLinks.map((link) => {
+        const isActive = pathname === link.route;
+
+        return (
+            <Link
+              to={link.route}
+              key={link.label} 
+              className={`${isActive && 'bg-primary-500 rounded-[10px]'} flex-center flex-col gap-1 p-2 transition`}
+            >
+              <img
+                    // src={link.imgURL}
+                    alt={link.label}
+                    width={16}
+                    height={16}
+                    className={`${isActive && 'invert-white'}`}
+                  />
+            </Link>
+        )
+      })}
+    </section>
   )
 }
 export default Bottombar
