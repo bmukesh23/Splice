45b3d09b6740a44874f22876b8fe1e6f6f995aed
Mukesh <billamukesh1234@gmail.com>
2024-02-16 00:28:11 +0530

    refactor: post-stats

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -1,4 +1,6 @@
 import { Models } from "appwrite";
+import LikeIcon from "/assets/icons/like.svg";
+import UnSavedIcon from "/assets/icons/save.svg";
 
 type PostStatsProps = {
     post: Models.Document;
@@ -7,7 +9,30 @@ type PostStatsProps = {
 
 const PostStats = ({post, userId}: PostStatsProps) => {
   return (
-    <div>PostStats</div>
+    <div className="flex justify-between items-center z-20">
+        <div className="flex gap-2 mr-5">
+            <img
+                src={LikeIcon}
+                alt="like"
+                width={20}
+                height={20}
+                onClick={()=>{}}
+                className="cursor-pointer"
+            />
+            <p className="small-medium lg:base-medium">0</p>
+        </div>
+
+        <div className="flex gap-2">
+            <img
+                src={UnSavedIcon}
+                alt="save"
+                width={20}
+                height={20}
+                onClick={()=>{}}
+                className="cursor-pointer"
+            />
+        </div>
+    </div>
   )
 }
 export default PostStats
