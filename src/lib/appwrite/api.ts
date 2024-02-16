39e9db85ba1d0695df516a5187dc70098923be2c
Mukesh <billamukesh1234@gmail.com>
2024-02-16 13:36:00 +0530

    refactor: api.ts

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -178,3 +178,58 @@ export const getRecentPosts = async () => {
 
     return posts;
 }
+
+export const likePost = async (postId: string, likesArray: string[]) => {
+    try {
+        const updatedPost = await databases.updateDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.postCollectionId,
+            postId,
+            {
+                likes: likesArray,
+            }
+        )
+
+        if (!updatedPost) throw Error;
+
+        return updatedPost;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const savePost = async (postId: string, userId: string) => {
+    try {
+        const updatedPost = await databases.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.savesCollectionId,
+            ID.unique(),
+            {
+                user: userId,
+                post: postId,
+            }
+        )
+
+        if (!updatedPost) throw Error;
+
+        return updatedPost;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const deletedSavedPost = async (savedRecordId: string) => {
+    try {
+        const statusCode = await databases.deleteDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.savesCollectionId,
+            savedRecordId,
+        )
+
+        if (!statusCode) throw Error;
+
+        return { status: 'ok' };
+    } catch (error) {
+        console.log(error);
+    }
+}
