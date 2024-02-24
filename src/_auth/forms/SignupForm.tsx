0fa93680002d004ca53a347a1116a708ae4dcc35
Mukesh <billamukesh1234@gmail.com>
2024-02-24 17:19:52 +0530

    refactor: signup-form.tsx

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -10,7 +10,6 @@ import { useToast } from "@/components/ui/use-toast";
 import { useCreateUserAccount, useSignInAccount } from "@/lib/react-query/queriesAndMutation";
 import { useUserContext } from "@/context/AuthContext";
 import Loader from "@/components/shared/Loader";
-import { account } from "@/lib/appwrite/config";
 
 
 const SignupForm = () => {
@@ -31,64 +30,6 @@ const SignupForm = () => {
       password: '',
     },
   })
-
-  const handleGoogleLogin = async () => {
-    try {
-      const res = await account.createOAuth2Session(
-        "google",
-        "http://localhost:5173/", // Redirect URI after Google authentication
-        "http://localhost:5173/sign-in" // Callback URL after Google authentication
-      );
-      
-      // Handle success or redirect
-      console.log(res);
-      
-      if (!res) {
-        throw new Error("Failed to authenticate with Google");
-      }
-      
-      let email: string | undefined;
-      let name: string | undefined;
-      let username: string | undefined;
-
-      // Check if res is a URL
-      if (res instanceof URL) {
-        // Extract email, name, and username from the URL if available
-        email = res.searchParams.get("email");
-        name = res.searchParams.get("name");
-        username = res.searchParams.get("username");
-      }
-
-      // Check if required data is available
-      if (!email) {
-        throw new Error("Email not provided by Google");
-      }
-      // Create a user account in Appwrite
-      const newUser = await createUserAccount({ email, name, username });
-
-      // Check if user creation was successful
-      if (!newUser) {
-        throw new Error("Failed to create user account");
-      }
-
-      // Sign in the user after account creation
-      const session = await signInAccount({
-        email: newUser.email,
-        password: newUser.username, // Password is not needed as the user is authenticated with OAuth2
-      });
-
-      // Check if sign-in was successful
-      if (!session) {
-        throw new Error("Failed to sign in user");
-      }
-
-      // Navigate to the desired page after successful sign-in
-      navigate('/');
-    } catch (error) {
-      console.error("Google authentication error:", error);
-      // Handle error appropriately
-    }
-  };
 
   const onSubmit = async (values: z.infer<typeof SignupValidation>) => {
 
@@ -181,14 +122,6 @@ const SignupForm = () => {
               </FormItem>
             )}
           />
-           <Button onClick={handleGoogleLogin} className="text-light-3">
-              <img
-                src="/assets/icons/google.svg"
-                alt="google"
-                className="mr-2"
-              />
-              Sign in with Google
-          </Button>
           <Button type="submit" className="shad-button_primary">
             {isCreatingAccount ? (
               <div className="flex-center gap-2">
@@ -199,7 +132,7 @@ const SignupForm = () => {
           <p className="text-small-regular text-light-2 text-center mt-2">
             Already have an account?
             <Link to='/sign-in' className="text-blue-400 text-small-semibold ml-1">Log in</Link>
-          </p>
+          </p> 
         </form>
       </div>
     </Form>
